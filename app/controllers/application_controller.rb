10fb469d101966dc165865ef846b16c02d2861dd
GardRyan <res.gardner@gmail.com>
2024-03-16 11:12:37 -0400

    updated application controller to include users and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,24 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def logged_in?
+    !!current_user
+  end
+  helper_method :logged_in?
+
+  def require_user
+    unless logged_in?
+      flash[:alert] = "Welcome to the jungle! You must identify if you're predator or prey."
+      redirect_to login_path
+    end
+  end
+  helper_method :require_user
+
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
