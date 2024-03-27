04cac65b5f6a7f4d47cf51d35f6f05c324ee7fac
GardRyan <res.gardner@gmail.com>
2024-03-27 17:09:01 -0400

    created tests for product_details

---
+++ cypress/integrations/product_details.spec.js
@@ -0,0 +1,15 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('displays the home page', () => {
+    cy.get(".products article").should("be.visible");
+  })
+
+  it("Navigates from the home page to a product", async () => {
+    cy.contains("Scented Blade").click()
+    cy.get('.btn').should("be.visible")
+  })
+  
+})
